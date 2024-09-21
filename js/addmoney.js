e276056f8400a35ea853641b93c6713ca568e121
fardinariful <fardinewucse2020@gmail.com>
2024-09-21 12:32:07 +0600

    something added

---
+++ js/addmoney.js
@@ -0,0 +1,24 @@
+
+function addmoney(){
+    
+
+
+}
+
+
+
+function hiddenelement(elementID){
+    const show=document.getElementById(elementID);
+    show.classList.add('hidden');
+}
+
+
+
+function showelement(elementID){
+    const show=document.getElementById(elementID);
+    show.classList.remove('hidden');
+}
+
+
+
+
